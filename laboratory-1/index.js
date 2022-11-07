2e9078d44b9c39484aeefb25b06e445c0a4a49ce
Roman Savoshchik <tweakice@gmail.com>
2022-11-07 21:23:34 +0600

    [-]Lab_1 - Delete extra comments

--- laboratory-1/index.js
+++ laboratory-1/index.js
@@ -3,7 +3,6 @@ let body = document.querySelector('body')
 let burger = document.querySelector('.header--burger')
 let headerMenu = document.querySelector('.header--nav')
 
-// Apply .active class to burger menu 
 burger.onclick = () => {
     burger.classList.toggle('active')
     headerMenu.classList.toggle('active')
