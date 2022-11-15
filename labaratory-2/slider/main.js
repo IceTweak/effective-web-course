ff9df880a72537c384fd0d9c05875c860a2f155b
Roman Savoshchik <tweakice@gmail.com>
2022-11-16 01:55:45 +0600

    Lab_2:feat - Structure changes for local storage

--- labaratory-2/slider/main.js
+++ labaratory-2/slider/main.js
@@ -1,10 +1,11 @@
-const slideContainer = document.querySelector('.container');
-const slide = document.querySelector('.slides');
-const nextBtn = document.getElementById('next');
-const prevBtn = document.getElementById('prev');
-const interval = 3000;
+const slideContainer = document.querySelector(".container");
+const slide = document.querySelector(".slides");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+const interval = 4000;
+const keyStorage = "currentSlideKey";
 
-const getSlides = () => document.querySelectorAll('.slide');
+const getSlides = () => document.querySelectorAll(".slide");
 
 let slides = getSlides();
 let index = 1;
@@ -13,8 +14,8 @@ let autoSlide;
 const firstClone = slides[0].cloneNode(true);
 const lastClone = slides[slides.length - 1].cloneNode(true);
 
-firstClone.id = 'first-clone';
-lastClone.id = 'last-clone';
+firstClone.id = "first-clone";
+lastClone.id = "last-clone";
 
 slide.append(firstClone);
 slide.prepend(lastClone);
@@ -22,57 +23,68 @@ slide.prepend(lastClone);
 const slideWidth = slides[index].clientWidth;
 
 const move = (slideTo) => {
-    slides = getSlides();
-    if (index >= slides.length - 1 || index <= 0) return;
-    slideTo === 'next' ? index++ : index--;
-    slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    slide.style.transition = '.7s';
+  slides = getSlides();
+  if (index >= slides.length - 1 || index <= 0) return;
+  slideTo === "next" ? index++ : index--;
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  slide.style.transition = ".7s";
+  clearInterval(autoSlide);
+  localStorage.setItem(keyStorage, index);
+  startSlide();
 };
 
 const startSlide = () => {
-    autoSlide = setInterval(() => {
-        move('next');
-    }, interval);
+  autoSlide = setInterval(() => {
+    move("next");
+  }, interval);
 };
-
 startSlide();
 
-slide.addEventListener('transitionend', () => {
-    slides = getSlides();
-    if (slides[index].id === firstClone.id) {
-        slide.style.transition = 'none';
-        index = 1;
-        slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    }
-    if (slides[index].id === lastClone.id) {
-        slide.style.transition = 'none';
-        index = slides.length - 2;
-        slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    }
+const infiniteHandle = () => {
+  slides = getSlides();
+  if (slides[index].id === firstClone.id) {
+    slide.style.transition = "none";
+    index = 1;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+  if (slides[index].id === lastClone.id) {
+    slide.style.transition = "none";
+    index = slides.length - 2;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+};
+
+slide.addEventListener("transitionend", () => {
+  infiniteHandle();
 });
 
-slideContainer.addEventListener('mouseenter', () => {
-    clearInterval(autoSlide);
+slideContainer.addEventListener("mouseenter", () => {
+  clearInterval(autoSlide);
 });
 
-slideContainer.addEventListener('mouseleave', startSlide);
+slideContainer.addEventListener("mouseleave", startSlide);
 
-nextBtn.addEventListener('click', () => {
-    move('next');
-    clearInterval(autoSlide);
-});
-prevBtn.addEventListener('click', () => {
-    move('prev');
-    clearInterval(autoSlide);
+nextBtn.addEventListener("click", () => move("next"));
+prevBtn.addEventListener("click", () => move("prev"));
+
+window.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "Space":
+      move("next");
+      break;
+    case "ArrowRight":
+      move("next");
+      break;
+    case "ArrowLeft":
+      move("prev");
+      break;
+  }
 });
 
-window.addEventListener('keyup', (event) => {
-    switch (event.code) {
-        case "Space": move('next');
-        break;
-        case "ArrowRight": move('next');
-        break;
-        case "ArrowLeft": move('prev');
-        break;
-    }
-})
+function checkStorage() {
+  slides = getSlides();
+  index = Number(localStorage.getItem(keyStorage) || 1);
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  infiniteHandle();
+}
+checkStorage();
